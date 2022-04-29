2625ab793d8656e54ca9c1bd94bd931ca2a848b4
千叶 <1348571853@qq.com>
2022-04-29 15:30:53 +0800

    Initial commit

---
+++ api/order/weixin.js
@@ -0,0 +1,23 @@
+import request from '@/utils/request'
+
+export default {
+  createNative(orderId) {
+    return request({
+      url: `/api/order/weixin/createNative/${orderId}`,
+      method: 'get'
+    })
+  },
+  queryPayStatus(orderId) {
+    return request({
+      url: `/api/order/weixin/queryPayStatus/${orderId}`,
+      method: 'get'
+    })
+  },
+  cancelOrder(orderId) {
+    return request({
+      url: `/api/order/orderInfo/auth/cancelOrder/${orderId}`,
+      method: 'get'
+    })
+  },
+
+}
